12ff8c066334316f252e31f3f2674bcbfd8de044
FiftySand <60965549+FiftySand@users.noreply.github.com>
2021-01-08 19:31:18 +0100

    pipeline test

--- KappersZaak/src/test/java/testRepository/accountRepository.java
+++ KappersZaak/src/test/java/testRepository/accountRepository.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class accountRepository {
-
+    //pipeline test
     public List<Account> accounts = new ArrayList<>();
 
     public void createAccount(Account account)
