f69fede91d6d7a0b99d2aef46eefe723a167ea0b
FiftySand <60965549+FiftySand@users.noreply.github.com>
2021-01-09 19:13:33 +0100

    poging6000

---
+++ KappersZaak/target/backend-0.0.1-SNAPSHOT/WEB-INF/classes/data.sql
@@ -0,0 +1,17 @@
+  INSERT INTO account (id, email, name , password, receive_email) VALUES
+  (1, 'testUser1@hotmail.com','testUser1', 'testUser1',1),
+  (2, 'testUser2@hotmail.com','testUser2', 'testUser2',0),
+  (3, 'testUser3@hotmail.com','testUser3', 'testUser3',1),
+  (4, 'testUser4@hotmail.com','testUser4', 'testUser4',0);
+
+  INSERT INTO kapper (id, name, age) VALUES
+  (1, 'kapper1',14),
+  (2, 'kapper2',12),
+  (3, 'kapper3',15),
+  (4, 'kapper4',16);
+
+  INSERT INTO appointment(id, fk_account, fk_kapper) VALUES
+  (1,1,1),
+  (2,2,1),
+  (3,3,4),
+  (4,4,3)
